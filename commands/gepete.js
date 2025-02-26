4394cc19053bcaa8c29d3441d4e0e607a8e1458d
donna267 <alliecetaperla@gmail.com>
2025-02-26 23:31:19 +0800

    Update gepete.js

--- commands/gepete.js
+++ commands/gepete.js
@@ -23,7 +23,7 @@ module.exports = {
       const response = await axios.get(apiUrl);
 
       if (response.data && response.data.reply) {
-        const reply = response.data.reply;
+        const reply = response.data.response;
 
         // Split the reply into chunks of 2000 characters or less
         const chunkSize = 2000;
